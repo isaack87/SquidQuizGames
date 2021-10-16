3ddcfd1340d75e996be7a4b6376627c1a532dd63
isaack87 <ijkim@asu.edu>
2021-10-15 19:14:44 -0700

    improvement to start screen CSS

--- client/src/components/mainScreen/selectionBox.jsx
+++ client/src/components/mainScreen/selectionBox.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams } from "react-router-dom";
+import styles from './selectionBox.module.css'
 
 
 class SelectionBox extends React.Component {
@@ -72,41 +73,40 @@ class SelectionBox extends React.Component {
     render () {
         return (
         <div className="fullscreenbox startbg">
-            <div className='registerbox'>
+            <div className='registerbox bodySelection'>
             <h1 className={this.state.playerPicked === false ?  'gametitle rainbow' : 'hide' }> Player Registration </h1>
         <div className={this.state.playerPicked === true ?  'shownamepicked rainbow'  : 'hide' }> Welcome Player {this.state.playerName}</div>
         <div className={this.state.playerPicked === true ? 'hidemargin hide' : 'showname' }>
         <form>
-        <input type="text" className="barlength signnamebar" placeholder="Enter Name Here .." onChange={this.handleChange}/>
-        <button value={this.state.playerName} onClick={this.handleSubmit} className="lockin"> submit </button>
+        <input type="text" className="barlength signnamebar rainbow" placeholder="Enter Name Here .." onChange={this.handleChange}/>
+        <button value={this.state.playerName} onClick={this.handleSubmit} className="lockin rainbow"> submit </button>
         </form>
          </div>
 
-    <div id="menu">
-        <ul>
 
+    <div id="menu">
+        <ul className="containerMenu">
         <li><a href="#"><div id={this.state.genreSelected === true ? ['c-selected'] : ['icon-top'] } className="icon-chrome1"></div> <div className="title-top">Category</div></a>
             <ul>
-                <li><a href="#" onClick={this.onChangeGenre.bind(this, 27)}><div id="icon" className="icon-pie"></div> <div className="title">Animals</div></a></li>
-                <li><a href="#" onClick={this.onChangeGenre.bind(this, 21)}><div id="icon" className="icon-pie"></div> <div className="title">Sports</div></a></li>
-                <li><a href="#" onClick={this.onChangeGenre.bind(this, '')}><div id="icon" className="icon-pie"></div> <div className="title">Random</div></a></li>
-                <li><a href="#" onClick={this.onChangeGenre.bind(this, 23)}><div id="icon" className="icon-pie"></div> <div className="title">History</div></a></li>
+                <li><a href="#" onClick={this.onChangeGenre.bind(this, 27)}><div id="icon" className="icon-pie1"></div> <div className="title">Animals</div></a></li>
+                <li><a href="#" onClick={this.onChangeGenre.bind(this, 21)}><div id="icon" className="icon-pie2"></div> <div className="title">Sports</div></a></li>
+                <li><a href="#" onClick={this.onChangeGenre.bind(this, '')}><div id="icon" className="icon-pie3"></div> <div className="title">Random</div></a></li>
             </ul>
         </li>
 
         <li><a href="#"><div id={this.state.difficultySelected === true ? ['c-selected'] : ['icon-top'] } className="icon-chrome2"></div> <div className="title-top">Difficulty</div></a>
             <ul>
-                <li><a href="#" onClick={this.onChangeDifficulty.bind(this, 'easy')}><div id="icon" className="icon-pie"></div> <div className="title">Easy</div></a></li>
-                <li><a href="#" onClick={this.onChangeDifficulty.bind(this, 'medium')}><div id="icon" className="icon-pie"></div> <div className="title">Medium</div></a></li>
-                <li><a href="#" onClick={this.onChangeDifficulty.bind(this, 'hard')}><div id="icon" className="icon-pie"></div> <div className="title">Hard</div></a></li>
+                <li><a href="#" onClick={this.onChangeDifficulty.bind(this, 'easy')}><div id="icon" className="icon-pie5"></div> <div className="title">Easy</div></a></li>
+                <li><a href="#" onClick={this.onChangeDifficulty.bind(this, 'medium')}><div id="icon" className="icon-pie5"></div> <div className="title">Medium</div></a></li>
+                <li><a href="#" onClick={this.onChangeDifficulty.bind(this, 'hard')}><div id="icon" className="icon-pie5"></div> <div className="title">Hard</div></a></li>
             </ul>
         </li>
 
         <li><a href="#"><div id={this.state.roundsSelected === true ? ['c-selected'] : ['icon-top'] } className="icon-chrome3"></div> <div className="title-top">Rounds</div></a>
             <ul>
-                <li><a href="#" onClick={this.onChangeRounds.bind(this, '5')}><div id="icon" className="icon-pie"></div> <div className="title">5 Rnds</div></a></li>
-                <li><a href="#" onClick={this.onChangeRounds.bind(this, '10')}><div id="icon" className="icon-pie"></div> <div className="title">10 Rnds</div></a></li>
-                <li><a href="#" onClick={this.onChangeRounds.bind(this, '50')}><div id="icon" className="icon-pie"></div> <div className="title">Unlimited</div></a></li>
+                <li><a href="#" onClick={this.onChangeRounds.bind(this, '5')}><div id="icon" className="icon-pie7"></div> <div className="title">5 Rnds</div></a></li>
+                <li><a href="#" onClick={this.onChangeRounds.bind(this, '10')}><div id="icon" className="icon-pie8"></div> <div className="title">10 Rnds</div></a></li>
+                <li><a href="#" onClick={this.onChangeRounds.bind(this, '50')}><div id="icon" className="icon-pie9"></div> <div className="title">Unlimited</div></a></li>
             </ul>
         </li>
     </ul>
--- client/src/components/mainScreen/startPage.jsx
+++ client/src/components/mainScreen/startPage.jsx
@@ -4,14 +4,12 @@ import SelectionBox from './selectionBox.jsx';
 
 class StartPage extends React.Component {
 
-
     render() {
         return (
-            
             <div className="center">
                 <SelectionBox />
             </div>
-        
+
         )
     }
 }
